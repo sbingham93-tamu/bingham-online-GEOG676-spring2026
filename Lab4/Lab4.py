8940853762a14fa9996468ede90e042e291fbe69
sbingham93 <sbingham93@tamu.edu>
2026-02-20 16:59:58 -0600

    Add GDB existence check and fix all path escape warnings

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -6,26 +6,26 @@ arcpy.env.workspace = r'C:\Users\sbing\TAMU\GEOG676\bingham-online-GEOG676-sprin
 folder_path = r'C:\Users\sbing\TAMU\GEOG676\bingham-online-GEOG676-spring2026\Lab4'
 gdb_name = 'Lab04.gdb'
 gdb_path = folder_path + '\\' + gdb_name
-arcpy.CreateFileGDB_management(folder_path, gdb_name)
+import os
+if not os.path.exists(gdb_path):
+	arcpy.CreateFileGDB_management(folder_path, gdb_name)
 
 csv_path = r'C:\Users\sbing\TAMU\GEOG676\TAMU-MGSc-Online-GEOG676-GIS-PROGRAMMING\data\homework\04\\garages.csv'
 garage_layer_name = 'Garage_Points'
 garages = arcpy.MakeXYEventLayer_management(csv_path, 'X', 'Y', garage_layer_name)
 
-input_layer = garages
-arcpy.FeatureClassToGeodatabase_conversion(input_layer, gdb_path)
 garage_points = gdb_path + '\\' + garage_layer_name
 
 # open campus gdb, copy building feature to our gdb
 campus = r'C:C:\Users\sbing\TAMU\GEOG676\TAMU-MGSc-Online-GEOG676-GIS-PROGRAMMING\data\homework\04\Campus.gdb'
-buildings_campus = campus + '\Structures'
-buildings = gdb_path + '\\' + 'Buildings'
+buildings_campus = campus + r'\Structures'
+buildings = gdb_path + r'\\' + 'Buildings'
 
 arcpy.Copy_management(buildings_campus, buildings)
 
 # Re-Projection
 spatial_ref = arcpy.Describe(buildings).spatialReference
-arcpy.Project_management(garage_points, gdb_path + '\Garage_Points_reprojected', spatial_ref)
+arcpy.Project_management(garage_points, gdb_path + r'\Garage_Points_reprojected', spatial_ref)
 
 # buffer the garages
 garageBuffered = arcpy.Buffer_analysis(gdb_path + r'\Garage_Points_reprojected', gdb_path + r'\Garage_Points_buffered', 150)
