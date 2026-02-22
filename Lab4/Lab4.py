d6e55d694efde94397a1539e048322e23c68b8b6
sbingham93 <sbingham93@tamu.edu>
2026-02-21 22:47:56 -0600

    Commit before rebase

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -28,9 +28,17 @@ spatial_ref = arcpy.Describe(buildings).spatialReference
 arcpy.Project_management(garage_points, gdb_path + r"\Garage_Points_reprojected", spatial_ref)
 
 # buffer the garages
-garageBuffered = arcpy.Buffer_analysis(gdb_path + r"\Garage_Points_reprojected", gdb_path + r"\Garage_Points_buffered", 150)
+garageBuffered = arcpy.Buffer_analysis(
+gdb_path + r"\Garage_Points_reprojected",
+gdb_path + r"\Garage_Points_buffered",
+buffer_distance
+)
 
 # Intersect our buffer with the buildings
 arcpy.Intersect_analysis([garageBuffered, buildings], gdb_path + r"\Garage_Building_Intersection", 'ALL')
 
-arcpy.TableToTable_conversion(gdb_path + r"\Garage_Building_Intersection.dbf", r'C:\Users\sbing\TAMU\GEOG676\bingham-online-GEOG676-spring2026\Lab4', 'nearbyBuildings.csv')
+arcpy.TableToTable_conversion(
+gdb_path + r"\Garage_Building_Intersect",
+r"C:\Users\sbing\TAMU\GEOG676\bingham-online-GEOG676-spring2026\Lab5",
+"nearbyBuildings"
+)
