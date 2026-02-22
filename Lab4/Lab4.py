81137bdee48eebae7ede2c87a969779a7cc6f32e
sbingham93 <sbingham93@tamu.edu>
2026-02-21 22:45:42 -0600

    Lab4 updates

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -18,19 +18,19 @@ garage_points = gdb_path + '\\' + garage_layer_name
 
 # open campus gdb, copy building feature to our gdb
 campus = r'C:\Users\sbing\TAMU\GEOG676\TAMU-MGSc-Online-GEOG676-GIS-PROGRAMMING\data\homework\04\Campus.gdb'
-buildings_campus = campus + r'\Structures'
-buildings = gdb_path + r'\\' + 'Buildings'
+buildings_campus = campus + r"\Structures"
+buildings = gdb_path + r"\\" + 'Buildings'
 
 arcpy.Copy_management(buildings_campus, buildings)
 
 # Re-Projection
 spatial_ref = arcpy.Describe(buildings).spatialReference
-arcpy.Project_management(garage_points, gdb_path + r'\Garage_Points_reprojected', spatial_ref)
+arcpy.Project_management(garage_points, gdb_path + r"\Garage_Points_reprojected", spatial_ref)
 
 # buffer the garages
-garageBuffered = arcpy.Buffer_analysis(gdb_path + r'\Garage_Points_reprojected', gdb_path + r'\Garage_Points_buffered', 150)
+garageBuffered = arcpy.Buffer_analysis(gdb_path + r"\Garage_Points_reprojected", gdb_path + r"\Garage_Points_buffered", 150)
 
 # Intersect our buffer with the buildings
-arcpy.Intersect_analysis([garageBuffered, buildings], gdb_path + r'\Garage_Building_Intersection', 'ALL')
+arcpy.Intersect_analysis([garageBuffered, buildings], gdb_path + r"\Garage_Building_Intersection", 'ALL')
 
-arcpy.TableToTable_conversion(gdb_path + r'\Garage_Building_Intersection.dbf', r'C:\Users\sbing\TAMU\GEOG676\bingham-online-GEOG676-spring2026\Lab4', 'nearbyBuildings.csv')
+arcpy.TableToTable_conversion(gdb_path + r"\Garage_Building_Intersection.dbf", r'C:\Users\sbing\TAMU\GEOG676\bingham-online-GEOG676-spring2026\Lab4', 'nearbyBuildings.csv')
