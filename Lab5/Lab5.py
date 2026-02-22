dfd9acce546164f52b599ade707dda9b55ce8d30
sbingham93 <sbingham93@tamu.edu>
2026-02-22 09:55:31 -0600

    Commit and push all changes

--- Lab5/Lab5.py
+++ Lab5/Lab5.py
@@ -121,7 +121,7 @@ class BuildingProximity(object):
 
         arcpy.TableToTable_conversion(
             gdb_path + r"\Garage_Building_Intersect",
-            r"C:\Users\sbing\TAMU\GEOG676\bingham-online-GEOG676-spring2026\Lab5",
+            r"C:\Users\sbing\TAMU\GEOG676\TAMU-MGSc-Online-GEOG676-GIS-PROGRAMMING\data\homework\04\Campus.gdb",
             "nearbyBuildings"
         )
                                       
